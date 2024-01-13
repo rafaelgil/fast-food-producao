ac6e155413e26153f91a3ed285bf5a4379c64255
Felipe Ferreira de Sales <felipe.ferreira.sales@gmail.com>
2024-01-13 07:44:16 -0300

    config da remoção da mensagem após o consume

--- src/main/java/br/com/fiap/postech/fastfoodproducao/infraestructure/AwsSQSConfig.java
+++ src/main/java/br/com/fiap/postech/fastfoodproducao/infraestructure/AwsSQSConfig.java
@@ -1,18 +1,27 @@
 package br.com.fiap.postech.fastfoodproducao.infraestructure;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import io.awspring.cloud.sqs.listener.acknowledgement.AcknowledgementResultCallback;
+import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
+import java.time.OffsetDateTime;
+import java.util.Collection;
+
 @Configuration
 public class AwsSQSConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AwsSQSConfig.class);
 
     @Value("${spring.cloud.aws.credentials.access-key}")
     private String accessKey;
@@ -38,9 +47,25 @@ public class AwsSQSConfig {
     }
 
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
+    public SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
+
+        return SqsMessageListenerContainerFactory.builder()
+                .configure(options -> options.acknowledgementMode(AcknowledgementMode.ON_SUCCESS))
+                .acknowledgementResultCallback(new AckResultCallback())
+                .sqsAsyncClient(sqsAsyncClient)
+                .build();
+    }
+
+    static class AckResultCallback implements AcknowledgementResultCallback<Object> {
+
+        @Override
+        public void onSuccess(Collection<Message<Object>> messages) {
+            LOGGER.info("Ack with success at {}", OffsetDateTime.now());
+        }
+
+        @Override
+        public void onFailure(Collection<Message<Object>> messages, Throwable t) {
+            LOGGER.error("Ack with fail", t);
+        }
     }
 }
