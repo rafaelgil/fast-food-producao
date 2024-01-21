dd792839e448f3cb17eea8873144a6f7e73f67c4
Felipe Ferreira de Sales <felipe.ferreira.sales@gmail.com>
2024-01-21 07:11:46 -0300

    Testes consumer

--- src/main/java/br/com/fiap/postech/fastfoodproducao/presentation/consumer/PedidoConsumer.java
+++ src/main/java/br/com/fiap/postech/fastfoodproducao/presentation/consumer/PedidoConsumer.java
@@ -3,21 +3,22 @@ package br.com.fiap.postech.fastfoodproducao.presentation.consumer;
 import br.com.fiap.postech.fastfoodproducao.application.service.PedidoService;
 import br.com.fiap.postech.fastfoodproducao.dto.PedidoDto;
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Service
+@RequiredArgsConstructor
 public class PedidoConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(PedidoConsumer.class);
 
-    @Autowired
-    private PedidoService pedidoService;
+//    @Autowired
+    private final PedidoService pedidoService;
 
     @SqsListener("fastfood-pedido")
     public void recieveMessage(Message<PedidoDto> message) {
